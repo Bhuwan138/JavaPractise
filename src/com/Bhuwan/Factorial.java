7631c6a7b8f0089696756a095b333979dba85cc2
Bhuwan <pandeybhuwan138@gmail.com>
2021-08-21 00:40:53 +0545

    Factorial program added

---
+++ src/com/Bhuwan/Factorial.java
@@ -0,0 +1,16 @@
+package com.Bhuwan;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int fact = 1,num;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        num = s.nextInt();
+        for (int i=1;i<=num;i++){
+            fact *= i;
+        }
+        System.out.println("Factorial of " + num + " is " + fact);
+    }
+}
