f724e1352db5d0833934f8487d1a87628e7177c7
Bhuwan <pandeybhuwan138@gmail.com>
2021-10-12 00:11:34 +0545

    Greating in different language program added

---
+++ src/com/Other/Polymorfizm/Greating/German.java
@@ -0,0 +1,7 @@
+package com.Other.Polymorfizm.Greating;
+
+public class German extends Language{
+    public void greating(){
+        System.out.println("Guten Morgen zusammen, in German Language");
+    }
+}
---
+++ src/com/Other/Polymorfizm/Greating/Nepali.java
@@ -0,0 +1,7 @@
+package com.Other.Polymorfizm.Greating;
+
+public class Nepali extends Language{
+    public void greating(){
+        System.out.println("सबैलाइ शुभप्रभात, in Nepali Language");
+    }
+}
